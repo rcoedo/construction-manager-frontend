1ed0a5f5fe96e8b59cfc978fd7100453f5613127
Roman Coedo <romancoedo@gmail.com>
2022-03-04 18:34:29 +0100

    remove backend, promote frontend

--- src/pages/companyList/results/CompanyCard.tsx
+++ src/pages/companyList/results/CompanyCard.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { Company } from "../../../interfaces";
 import { Highlight } from "../../../components/common";
-import { Breakpoint } from "../../../utils";
 
 const CompanyCardContainer = styled.div`
   display: grid;
